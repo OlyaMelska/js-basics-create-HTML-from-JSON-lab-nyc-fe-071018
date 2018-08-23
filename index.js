5e0cc6877d2b775ea2c9a887e78ee3c356fd94ff
Olha Melska <olya.melska@gmail.com>
2018-08-23 23:31:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function updateHTMLContent(){
   let genre = document.getElementById('genre')
   let filmRating = document.getElementById('filmRating')
   
-  title.innerHTML = 'hi'
+  title.innerHTML = '<p>hi</p><p>Hello</p>'
 }
 
