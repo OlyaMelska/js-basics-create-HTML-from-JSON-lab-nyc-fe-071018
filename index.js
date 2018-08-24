266008d8b6231e2f4b1013534dfa0f6a2801ff42
Olha Melska <olya.melska@gmail.com>
2018-08-24 00:06:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ function updateHTMLContent(movieName){
   let castInnerHTML = '<ul>'
   
   for(castMember of currentMovie.cast){
-    let role = currentMovie.cast.castMember.role
-    let actor = currentMovie.cast.castMember.actor
+    let role = currentMovie.castMember.role
+    let actor = currentMovie.castMember.actor
     cast.innerHTML += ('<li>' + role + ": " + actor + '</li>')
   }
   castInnerHTML += '</ul>'
